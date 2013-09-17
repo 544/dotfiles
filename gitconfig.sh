374d81fe83feb1fcaa6a4ee52a2d245110044816
masato.tsukagoshi <masato.tsukagoshi@dena.com>
2013-09-17 21:19:37 +0900

    graph 見直し

--- gitconfig.sh
+++ gitconfig.sh
@@ -9,7 +9,9 @@
 git config --global core.editor vi
 
 # log diff 利用時のページャ
-#git config --global core.pager lv
+# nkfかますとうまく動かない・・・
+#git config --global core.pager 'lv -c | nkf -w -x'
+git config --global core.pager 'lv -c '
 
 # 色設定
 git config --global color.ui true
@@ -24,6 +26,36 @@ git config --global alias.stt status -uno
 # 行ごとの差分じゃなくて，単語レベルでの差分を色付きで表示する
 git config --global alias.difff diff --word-diff
 # いい感じのグラフでログを表示
+git config --global alias.graph 'log --graph --date-order -C -M --pretty=format:"<%h> %ad %Cgreen%d%Creset %s (%Cred%an%Creset)" --date=local'
+git config --global alias.graph-all 'log --graph --date-order -C -M --pretty=format:"<%h> %ad %Cgreen%d%Creset %s (%Cred%an%Creset)" --date=local --all'
 #git config --global alias.graph log --graph --date=short --decorate=short --pretty=format:'%Cgreen%h %Creset%cd %Cblue%cn %Cred%d %Creset%s'
 # 上の省略形
 #git config --global alias.gr log --graph --date=short --decorate=short --pretty=format:'%Cgreen%h %Creset%cd %Cblue%cn %Cred%d %Creset%s'
+
+exit
+
+# 以下メモ
+#color.diff=auto
+#color.interactive=auto
+#color.status=auto
+#color.branch=auto
+#color.ui=true
+#alias.st=status
+#alias.ci=commit
+#alias.co=checkout
+#alias.w=whatchanged
+#alias.b=branch
+#alias.dc=diff --cached
+#alias.url=config --get remote.origin.url
+#alias.graph=log --graph --date-order -C -M --pretty=format:"<%h> %ad %Cgreen%d%Creset %s (%Cred%an%Creset)" --date=local
+#alias.graph-all=log --graph --date-order -C -M --pretty=format:"<%h> %ad %Cgreen%d%Creset %s (%Cred%an%Creset)" --date=local --all
+#push.default=tracking
+#core.pager=lv -c | nkf -w -x
+#core.repositoryformatversion=0
+#core.filemode=true
+#core.bare=false
+#core.logallrefupdates=true
+#remote.origin.fetch=+refs/heads/*:refs/remotes/origin/*
+#remote.origin.url=https://github.com/544/dotfiles.git
+#branch.master.remote=origin
+#branch.master.merge=refs/heads/master
